7aa0ce20e0b48871c9b7e66d90098e2448674120
Surbhi Sharma <149923133+SurbhiSharma178@users.noreply.github.com>
2026-07-11 11:53:34 +0530

    Time: 10 ms (12.33%), Space: 26.9 MB (8.41%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -13,8 +13,9 @@ bool checkPalindrome(string s, int i,int j){
 
 public:
     bool validPalindrome(string s) {
+        int n= s.length();
         int i=0;
-        int j= s.length()-1;
+        int j= n-1;
         while(i<=j){
             if(s[i]!= s[j]){
                 return checkPalindrome(s, i+1, j) || checkPalindrome(s, i, j-1);
