18cf922a0ae9c10957430b8ca1e1de9814abfe02
Surbhi Sharma <149923133+SurbhiSharma178@users.noreply.github.com>
2026-07-15 17:59:08 +0530

    Time: 0 ms (100%), Space: 9.3 MB (57.18%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int hash[256]={0};
+        int tCharMapped[256]={0};
+        for(int i=0; i<s.length();i++){
+            if(hash[s[i]]==0 && tCharMapped[t[i]]==0){
+                hash[s[i]]=t[i];
+                tCharMapped[t[i]]=true;
+            }
+        }
+         for( int i=0;i<s.length();i++){
+                if(hash[s[i]]!= t[i]) return false;
+            }
+        return true;
+    }
+};
