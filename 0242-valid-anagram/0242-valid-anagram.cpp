1186060ecfa7bc5ad19988571ac95ba5cb1848c9
Surbhi Sharma <149923133+SurbhiSharma178@users.noreply.github.com>
2026-07-15 17:14:08 +0530

    Time: 0 ms (100%), Space: 9.7 MB (49.94%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        int frequencyTable[256]={0};
+        for(int i=0; i<s.size();i++){
+            frequencyTable[(int)s[i]]++;   
+        }
+        for(int i=0; i<t.length();i++){
+            frequencyTable[t[i]]--;
+        }
+        for(int i=0; i<256;i++){
+            if(frequencyTable[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
